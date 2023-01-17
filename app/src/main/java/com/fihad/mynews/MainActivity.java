e92a940d59ba4dd2bc5a194eff47e0315cec699b
fihad247 <fihadinfo@gmail.com>
2023-01-17 18:25:37 +0600

    MY 1st News App

---
+++ app/src/main/java/com/fihad/mynews/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fihad.mynews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
